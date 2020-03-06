f49668da666edb5ea2b9ca312ac1559626784e2b
Alina Osv <hixid@ya.ru>
2020-03-06 22:39:20 +0300

    Improved about description

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,6 +5,9 @@ function About() {
         <div>
             Hi, I'm Alina <span role="img" aria-label="girl emoji">👱🏻‍♀️</span> I'm a frontend dev, currently building interfaces at 
             <a href="https://yandex.ru">Yandex</a>
+            Recently I worked in DigDes - leading software development company.
+            I'm obsessed with yummy animations, clean code & elegant solutions.
+            Want to cooperate or just say hi - drop me a message on hixid@ya.ru.
         </div>
     );
 }
