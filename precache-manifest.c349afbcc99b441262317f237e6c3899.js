2871b0d648706f495ef2c367f7e0d0f24c95e6d6
Alina Osv <hixid@ya.ru>
2020-08-15 14:22:28 +0300

    Updates

---
+++ precache-manifest.c349afbcc99b441262317f237e6c3899.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4370abcd96c8199e5e9326f9af2c5b0c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "397301578b21fe40a662",
+    "url": "/static/css/main.b5002956.chunk.css"
+  },
+  {
+    "revision": "d90895d482492e928fe7",
+    "url": "/static/js/2.cdda38f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cdda38f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "397301578b21fe40a662",
+    "url": "/static/js/main.e0f60cfb.chunk.js"
+  },
+  {
+    "revision": "7a0c3c5a54b5dc27e829",
+    "url": "/static/js/runtime-main.a003e98e.js"
+  },
+  {
+    "revision": "334e3e676273fb7bfbb3ebe3bf62e48e",
+    "url": "/static/media/avatar.334e3e67.png"
+  },
+  {
+    "revision": "e48514de9487e43fb7815645637d3283",
+    "url": "/static/media/github.e48514de.svg"
+  },
+  {
+    "revision": "1ab390b7f86cdd2c0d184375d463837b",
+    "url": "/static/media/linkedin.1ab390b7.svg"
+  },
+  {
+    "revision": "367c0953b74c011d79987d89f2d12766",
+    "url": "/static/media/telegram.367c0953.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.10e4b522b4cfdcd72980af5a2e746007.js"
+  "/precache-manifest.c349afbcc99b441262317f237e6c3899.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
